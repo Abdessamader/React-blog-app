d15737ebec343705ab1030c9decf7d615e0bb540
abdessamader <abdessamaderrifi46@gmail.com>
2022-09-07 14:39:16 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 
 
 
 function App() {
-  const [title,setTitle] = useState("blog")
+
 
 
   return (
@@ -13,8 +13,7 @@ function App() {
       <Navbar/>
     <div className="content">
       <Home/>
-      <h1>welcome to my {title}</h1>
-      <button style={{marginTop:"3rem"}} onClick={()=>setTitle('Website')}>Change it</button>
+     
     </div>
     </div>
   );
