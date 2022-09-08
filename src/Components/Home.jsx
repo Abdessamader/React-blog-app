190754d667651917374fcb4ab10c08e9671094ed
abdessamader <abdessamaderrifi46@gmail.com>
2022-09-08 10:42:45 +0700

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
@@ -10,12 +11,7 @@ const Home = () => {
     
     return ( 
         <div className="Home">
-        {blogs.map((blog)=>(
-            <div className="blog-preview"  key={blog.id}>
-                <h2 style={ {color:"#f1356d", marginTop: "1rem", marginBottom : "1rem" } }> { blog.title } </h2>
-                <p>Written By {blog.author} </p>
-            </div>
-        ))}
+       <BlogList />
           
         </div>
      );
