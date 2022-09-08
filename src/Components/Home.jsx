ccb57d41866001733b9edfac576d1f189b0ae405
abdessamader <abdessamaderrifi46@gmail.com>
2022-09-08 23:52:04 +0700

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 
@@ -8,10 +8,21 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
-    
+   
+    useEffect(() => {
+        console.log('useEffect ran ');
+        console.log(blogs );
+    })
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className="Home">
-       <BlogList />
+            
+       <BlogList blogs = { blogs } title = "All Blogs" handleDelete = {handleDelete}    />
+       <BlogList blogs = { blogs.filter((blog)=>blog.author==="yoshi" ) } title = "Yagami's  Blogs"/>
           
         </div>
      );
