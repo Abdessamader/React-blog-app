a01464511bafc710a05330d7d4270c43b2d82249
abdessamader <abdessamaderrifi46@gmail.com>
2022-09-21 14:15:56 +0700

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,28 +2,44 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    // const handleDelete = (id) => {
-    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
-    //     setBlogs(newBlogs);
-    //   };
-  
-    useEffect(() => {
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
+  // const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //     setBlogs(newBlogs);
+  //   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          console.log(res);
+          if(!res.ok) {
+              throw Error('Could not fetch the data !!!')
+          }
           return res.json();
         })
-        .then(data => {
+        .then((data) => {
           setBlogs(data);
+          setIsPending(false);
+          setError(null)
         })
-    }, [])
-  
-    return (
-      <div className="home">
-        {blogs && <BlogList blogs={blogs} title = "all blogs" />}
-        {/* <BlogList blogs = { blogs.filter((blog)=>blog.author==="yoshi" ) } title = "Yagami's  Blogs"/> */}
-      </div>
-    );
-  }
-   
-  export default Home;
+        .catch((err)=>{
+          setIsPending(false)
+          setError(err.message)
+        })
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="home">
+      { error && <div> { error } </div> }
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="all blogs" />}
+      {/* <BlogList blogs = { blogs.filter((blog)=>blog.author==="yoshi" ) } title = "Yagami's  Blogs"/> */}
+    </div>
+  );
+};
+
+export default Home;
