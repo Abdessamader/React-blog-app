55462d30c4e4ec66a02229406ad69410727e77b6
abdessamader <abdessamaderrifi46@gmail.com>
2022-10-05 14:33:10 +0700

    commit

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { useState } from "react";
+import {useHistory} from 'react-router-dom'
 
 function Create() {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('Mario')
+  const [isPending, setIsPending] = useState(false)
+  const history = useHistory();
+
+
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const blog = {title, body, author};
+      setIsPending(true)
+      fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+      }).then(()=>{
+        console.log('new blog added');
+        setIsPending(false)
+        // history.go(-1)
+        history.push('/')
+      })
+  }
   
   return (
     <div className="create">
       <h2>Add a New Blog!</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
         type="text"
@@ -32,10 +53,9 @@ function Create() {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog....</button>}
+
       </form>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>React Blog</h1>
+      <Link to='/'><h1 >React Blog</h1></Link>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog </Link>
